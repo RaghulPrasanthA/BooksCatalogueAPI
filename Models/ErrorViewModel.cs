a44f81bc35fd63ebe17f0a1a8774debedd9200cd
gokuldemaria <raghulprasanthannadurai@gmail.com>
2025-01-04 16:21:24 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BooksCatalogueAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
